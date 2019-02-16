09b1fa4c1564ea89406edd47f97cf0ca0a5d78c9
Filip Handzel <f.handzel@gmail.com>
2019-02-16 12:09:40 +0100

    git add typelogo.js, modal, mobile correct

---
+++ js/typelogo-mobile.js
@@ -0,0 +1,8 @@
+var typed = new Typed('#typing-element2', {
+        strings: ['Filip Handzel '],
+        typeSpeed: 100,
+        startDelay: 1000,
+        backDelay: 3000,
+        backSpeed: 80,
+        loop: true
+    })
---
+++ js/typelogo.js
@@ -0,0 +1,8 @@
+var typed = new Typed('#typing-element', {
+        strings: ['Filip Handzel - intermedia', 'Filip Handzel - robotic art', 'Filip Handzel - creative coding', 'Filip Handzel - software art'],
+        typeSpeed: 100,
+        startDelay: 1000,
+        backDelay: 3000,
+        backSpeed: 80,
+        loop: true
+    })
